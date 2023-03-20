2870d7c7e7c3064edeac67346e2391c6d7ddcc93
cecesolihin <49848882+cecesolihin@users.noreply.github.com>
2023-03-20 14:23:03 +0700

    add model & controller

---
+++ Source/OrderApp/OrderApp/Models/OrderDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrderApp.Models
+{
+    public class OrderDetail
+    {
+        public string OrderId { get; set; }
+        public string ProductId { get; set; }
+        public string Qty { get; set; }
+        public string SubTotal { get; set; }
+    }
+}
---
+++ Source/OrderApp/OrderApp/Models/OrderHeader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrderApp.Models
+{
+    public class OrderHeader
+    {
+        public int OrderNo { get; set; }
+        public string OrderId { get; set; }
+        public string OrderDate { get; set; }
+        public string CustomerId { get; set; }
+        public string RequiredDate { get; set; }
+        public string ShipName { get; set; }
+        public string TotalPrice { get; set; }
+    }
+}
